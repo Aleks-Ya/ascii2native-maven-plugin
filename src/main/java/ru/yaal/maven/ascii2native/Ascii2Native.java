c88f0d1dee036d523535d3297d5682b0cd071e19
Aleksey Yablokov <ya_al@bk.ru>
2015-05-31 13:57:15 +0300

    optimize ascii2native algorithm

--- src/main/java/ru/yaal/maven/ascii2native/Ascii2Native.java
+++ src/main/java/ru/yaal/maven/ascii2native/Ascii2Native.java
@@ -4,6 +4,7 @@ package ru.yaal.maven.ascii2native;
  * @author Aleksey Yablokov.
  */
 public final class Ascii2Native {
+    private static final int ASCII_LENGTH = 6;
 
     private Ascii2Native() {
     }
@@ -11,45 +12,31 @@ public final class Ascii2Native {
     /**
      * Converts given CharSequence into ASCII String.
      */
-    public static String nativeToAscii(CharSequence csq) {
-        if (csq == null) {
+    public static String nativeToAscii(String cs) {
+        if (cs == null) {
             return null;
         }
         StringBuilder sb = new StringBuilder();
-        String cs = csq.toString();
         int i = 0;
-        String six = getSixChars(i, cs);
-        while (six != null) {
-            if (six.startsWith("\\u")) {
-                Character c = ascii2native(six);
-                sb.append(c);
-                i = i + 6;
-            } else {
-                sb.append(cs.charAt(i));
-                i++;
-            }
-            six = getSixChars(i, cs);
-        }
-        sb.append(getLastChars(i, cs));
+        int asciiIndex;
+        do {
+            asciiIndex = cs.indexOf("\\u", i);
+            String noAscii = asciiIndex != -1 ? cs.substring(i, asciiIndex) : cs.substring(i, cs.length());
+            sb.append(noAscii);
+            i = i + noAscii.length();
+            String six = asciiIndex != -1 ? cs.substring(asciiIndex, asciiIndex + ASCII_LENGTH) : "";
+            sb.append(ascii2native(six));
+            i = i + ASCII_LENGTH;
+        } while (asciiIndex != -1);
         return sb.toString();
     }
 
-    private static String getSixChars(int index, String s) {
-        if (s.length() < index + 6) {
-            return null;
+    private static String ascii2native(String ascii) {
+        if (ascii.length() == ASCII_LENGTH && ascii.startsWith("\\u")) {
+            String num = ascii.substring(2);
+            return Character.toString((char) Integer.decode("#" + num).intValue());
+        } else {
+            return ascii;
         }
-        return s.substring(index, index + 6);
-    }
-
-    private static String getLastChars(int index, String s) {
-        return s.length() > index ? s.substring(index) : "";
-    }
-
-    private static char ascii2native(String ascii) {
-        assert (ascii.length() == 6);
-        assert (ascii.startsWith("\\u"));
-
-        String num = ascii.substring(2);
-        return (char) Integer.decode("#" + num).intValue();
     }
 }
